cb406ce819c8fcb9d05b1cb2c6d7f38881cf6be6
dotrovi <136281137+dotrovi@users.noreply.github.com>
2023-11-10 10:15:51 +0800

    Create RegisterStyles.php

---
+++ RegisterStyles.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<style>
+  body 
+    {
+      background-color: #AAF35C;
+			background-image:url('backgroundmenuSK.jpg');
+			background-repeat: no repeat;
+			background-attachment: fixed;
+			background-size: 100% 100%;
+		}
+  
+  .w3-audiowide 
+    {
+			font-family: 'Audiowide', sans-serif;
+		}
+
+  .b 
+  {
+  font-family: Arial, Helvetica, sans-serif;
+  }
+</style>
