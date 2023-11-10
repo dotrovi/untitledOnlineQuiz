a2953e8185549ad8ac88d0a657a06fc017ab20b1
dotrovi <136281137+dotrovi@users.noreply.github.com>
2023-11-10 10:30:20 +0800

    Update LoginProcess.php

--- LoginProcess.php
+++ LoginProcess.php
@@ -2,7 +2,7 @@
 include 'connection.php';
 session_start();
 
-// login process
+// login process student
 if($_GET['q'] == 1)
 {
   
@@ -24,7 +24,35 @@ if($_GET['q'] == 1)
 	else 
 	{
 		// ke laman index.php
+		echo "<script>alert('Login Failed!')</script>";
 		header("location:StudentLogin.php");
 	}
+}
+
+// login process teacher
+if ($_GET['q'] == 2) {
+
+  // variables on input
+  $username = $_POST['username'];
+  $katalaluan = $_POST['pwd'];
+  
+  // find data in database
+  $query = mysqli_query($con, "SELECT * FROM guru WHERE namapengguna = '$username' AND katalaluan = '$katalaluan'");
+	
+  if ($user = mysqli_fetch_array($query)) 
+  {
+	  
+  // idguru as session token
+  $_SESSION['idguru'] = $user['idguru'];
+  // to main page for teachers
+  header("Location: senarai.php?q=1");
+ } 
+  else 
+  {
+  // paparan jika gagal log masuk
+  echo "<script>alert('Login Failed!')</script>";
+  // ke laman index.php
+  header("location:TeacherLogin.php");
+  }
 }
 ?>
