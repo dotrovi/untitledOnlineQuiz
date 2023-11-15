18e2481a3ea3992b15224a91f92374724cfa9ec6
Ivor Barrie Jaffery <136281137+dotrovi@users.noreply.github.com>
2023-11-15 20:46:03 +0800

    Update LoginProcess.php
    add exit()

--- LoginProcess.php
+++ LoginProcess.php
@@ -23,34 +23,39 @@ if($_GET['q'] == 1)
 	} 
 	else 
 	{
+		echo "<script>alert('Login Failed!')</script>";
 		// ke laman index.php
 		header("location:StudentLogin.php");
+		exit();
 	}
 }
 
 // login process teacher
-if ($_GET['q'] == 2) {
+if ($_GET['q'] == 2) 
+{
 
-  // variables on input
-  $username = $_POST['username'];
-  $katalaluan = $_POST['pwd'];
+ 	// variables on input
+ 	$username = $_POST['username'];
+  	$katalaluan = $_POST['pwd'];
   
-  // find data in database
-  $query = mysqli_query($con, "SELECT * FROM guru WHERE namapengguna = '$username' AND katalaluan = '$katalaluan'");
+  	// find data in database
+  	$query = mysqli_query($con, "SELECT * FROM guru WHERE namapengguna = '$username' AND katalaluan = '$katalaluan'");
 	
-  if ($user = mysqli_fetch_array($query)) 
-  {
+  	if ($user = mysqli_fetch_array($query)) 
+  	{	
 	  
-  // idguru as session token
-  $_SESSION['idguru'] = $user['idguru'];
-  // to main page for teachers
-  header("Location:List.php?q=1");
- } 
-  else 
-  {
-  // paparan jika gagal log masuk
-  // ke laman index.php
-  header("location:TeacherLogin.php");
-  }
+  		// idguru as session token
+  		$_SESSION['idguru'] = $user['idguru'];
+  		// to main page for teachers
+  		header("Location:List.php?q=1");
+ 	} 
+  	else 
+  	{
+  		// paparan jika gagal log masuk
+	  	echo "<script>alert('Login Failed!')</script>";
+  		// ke laman index.php
+  		header("location:TeacherLogin.php");
+  		exit();
+  	}
 }
 ?>
