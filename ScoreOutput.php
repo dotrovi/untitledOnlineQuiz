5b30a9f11e763687bcfe494fecd46ad68b0dea1f
Mervyn Bee Zheng Cheng <148675768+MERVYN3567@users.noreply.github.com>
2023-11-15 22:21:17 +0800

    Update ScoreOutput.php

--- ScoreOutput.php
+++ ScoreOutput.php
@@ -1,6 +1,3 @@
-<!-- SISTEM APLIKASI KUIZ SAINS -->
-<!-- DISEDIAKAN OLEH :- ivorbarriejaffery@smkmatunggong -->
-<!-- FILE : ScoreOutput.php -->
 <style>
 .a {
 	text-shadow: 1px 1px #C5E2EE;
@@ -24,6 +21,7 @@ session_start();
 if(!isset($_SESSION['idguru'])) {
 	//ke laman logmasukguru.php untuk log masuk
 	header('location:TeacherLogin.php');
+	exit();
 }
 ?>
 
